6b7455aee61a61d155f5ea416349c2353de50478
Andrey <andreyweb30@gmail.com>
2021-03-25 20:12:50 +0200

    zz

--- test20html.php
+++ test20html.php
@@ -173,6 +173,37 @@ if (isset($_GET['download']) and $_GET['download'] == 'true') {
 			input::placeholder {
 				color: #868F96;
 			}
+			.custom-checkbox {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+  }
+  .custom-checkbox+label {
+    display: inline-flex;
+    align-items: center;
+    user-select: none;
+  }
+  .custom-checkbox+label::after {
+    content: '';
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    flex-shrink: 0;
+    flex-grow: 0;
+    border: 2px solid #EBEDEF;
+    border-radius: 0.35em;
+    margin-right: 0.5em;
+	margin-left: 40px;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 50% 50%;
+  }
+
+  .custom-checkbox:checked+label::after {
+    border-color: #0b76ef;
+    background-color: #0b76ef;
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+  }
 		</style>
 	</head>
 
@@ -314,15 +345,15 @@ if (isset($_GET['download']) and $_GET['download'] == 'true') {
 
 						<label>Placeholder<input type="text" name="username"> </label>
 
-						<label>Required<input type="checkbox">
-
-						</label>
+						
+						<input type="checkbox" class="custom-checkbox" id="happy" name="happy" value="yes">
+    					<label for="happy">Required</label>
 					</form>
 				</div>
 			</div>
 		</div>
 
-
+		
 
 
 		<script>
